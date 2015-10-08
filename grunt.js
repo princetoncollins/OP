93ce893bfa083190b17367143c7fd728eb5e052f
princetoncollins <collinsprinceton@gmail.com>
2015-10-08 16:24:05 -0600

    almost

--- grunt.js
+++ grunt.js
@@ -2,18 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      }
-    },
-    build: {
-      dist: {
-        src: 'public/*.js',
-        dest: 'public.min.js'
-      }
-    }
+      
   });
 
   // Load the plugin that provides the "uglify" task.
