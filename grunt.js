aea6a0b4381f2bc604918bd593656862e490dcf9
princetoncollins <collinsprinceton@gmail.com>
2015-10-31 00:15:07 -0600

    Close to finished.

--- grunt.js
+++ grunt.js
@@ -2,7 +2,18 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-      
+      pkg: grunt.file.readJSON('package.json'),
+      concat: {
+        dist: {
+          src: [
+            'app.js',
+            'server.js'
+          ],
+          dest: {
+            'build/onlineportfolio.js'
+          }
+        }
+      }
   });
 
   // Load the plugin that provides the "uglify" task.
