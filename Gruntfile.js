38d71667c8b7b94a8ff0aac1deed748ea2f020e5
princetoncollins <collinsprinceton@gmail.com>
2015-11-07 01:50:36 -0700

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,8 +40,9 @@ grunt.initConfig({
         },
   connect: {
     options: {
-      port: 1337
+      port: 1337,
       // base: 'public'
+      hostname: 'princetoncollins.com'
     }
   }
 });
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public'));
 });
 
 app.use(bodyParser.json());
